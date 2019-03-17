83b433b4a4780f26e3feb57e03a220dfdba37002
Nate Andersen <oknate@gmail.com>
2019-03-17 07:04:00 -0400

    git commit -m 'Issue #3040286 by oknate, Berdir: PluginsTest failing on 8.6.11' --author="oknate <oknate@471638.no-reply.drupal.org>"

--- tests/src/FunctionalJavascript/EntityBrowserWebDriverTestBase.php
+++ tests/src/FunctionalJavascript/EntityBrowserWebDriverTestBase.php
@@ -7,19 +7,14 @@ use Drupal\Core\Field\FieldStorageDefinitionInterface;
 use Drupal\file\Entity\File;
 use Drupal\field\Entity\FieldConfig;
 use Drupal\field\Entity\FieldStorageConfig;
-use Drupal\FunctionalJavascriptTests\JavascriptTestBase;
+use Drupal\FunctionalJavascriptTests\WebDriverTestBase;
 
 /**
  * Base class for Entity browser Javascript functional tests.
  *
  * @package Drupal\Tests\entity_browser\FunctionalJavascript
- *
- * @deprecated in Drupal 8.6.x, will be removed before Drupal 9.0.0.
- * Use \Drupal\Tests\entity_browser\FunctionalJavascript\EntityBrowserWebDriverTestBase instead
- *
- * @see \Drupal\FunctionalJavascriptTests\JavascriptTestBase for more info.
  */
-abstract class EntityBrowserJavascriptTestBase extends JavascriptTestBase {
+abstract class EntityBrowserWebDriverTestBase extends WebDriverTestBase {
 
   /**
    * Modules to enable.
@@ -179,64 +174,6 @@ abstract class EntityBrowserJavascriptTestBase extends JavascriptTestBase {
     $image->save();
 
     return $image;
-  }
-
-  /**
-   * Waits for jQuery to become ready and animations to complete.
-   */
-  protected function waitForAjaxToFinish() {
-    $this->assertSession()->assertWaitOnAjaxRequest();
-  }
-
-  /**
-   * Waits and asserts that a given element is visible.
-   *
-   * @param string $selector
-   *   The CSS selector.
-   * @param int $timeout
-   *   (Optional) Timeout in milliseconds, defaults to 1000.
-   * @param string $message
-   *   (Optional) Message to pass to assertJsCondition().
-   */
-  protected function waitUntilVisible($selector, $timeout = 1000, $message = '') {
-    $condition = "jQuery('" . $selector . ":visible').length > 0";
-    $this->assertJsCondition($condition, $timeout, $message);
-  }
-
-  /**
-   * Click on element found by xpath selector.
-   *
-   * @param string $xpathSelector
-   *   Xpath selector for element that will be used to trigger click on it.
-   * @param bool $waitAfterAction
-   *   Flag to wait after click is executed.
-   */
-  protected function clickXpathSelector($xpathSelector, $waitAfterAction = TRUE) {
-    $this->getSession()->getPage()->find('xpath', $xpathSelector)->click();
-
-    if ($waitAfterAction) {
-      $this->waitForAjaxToFinish();
-    }
-  }
-
-  /**
-   * Debugger method to save additional HTML output.
-   *
-   * The base class will only save browser output when accessing page using
-   * ::drupalGet and providing a printer class to PHPUnit. This method
-   * is intended for developers to help debug browser test failures and capture
-   * more verbose output.
-   */
-  protected function saveHtmlOutput() {
-    $out = $this->getSession()->getPage()->getContent();
-    // Ensure that any changes to variables in the other thread are picked up.
-    $this->refreshVariables();
-    if ($this->htmlOutputEnabled) {
-      $html_output = '<hr />Ending URL: ' . $this->getSession()->getCurrentUrl();
-      $html_output .= '<hr />' . $out;
-      $html_output .= $this->getHtmlOutputHeaders();
-      $this->htmlOutput($html_output);
-    }
   }
 
 }
